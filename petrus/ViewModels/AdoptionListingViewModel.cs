7937c18a383aa12240a65ec9dc808ccf7a942daf
Shirley <chowjss@gmail.com>
2021-08-13 12:46:08 +0800

    Update AdoptionListingViewModel.cs

--- petrus/ViewModels/AdoptionListingViewModel.cs
+++ petrus/ViewModels/AdoptionListingViewModel.cs
@@ -16,13 +16,18 @@ namespace petrus.ViewModels
         [Required(ErrorMessage = "Please enter age")]
         public int Age { get; set; }
         [Required(ErrorMessage = "Please enter breed")]
+        [Display(Name = "Primary Breed")]
         public Breed Breed1 { get; set; }
+        [Display(Name = "Secondary Breed")]
         public Breed? Breed2 { get; set; }
         [Required(ErrorMessage = "Please enter gender")]
         public Gender Gender { get; set; }
         [Required(ErrorMessage = "Please enter colour")]
+        [Display(Name = "Colour 1")]
         public Color Color1 { get; set; }
+        [Display(Name = "Colour 2 (optional)")]
         public Color? Color2 { get; set; }
+        [Display(Name = "Colour 3 (optional)")]
         public Color? Color3 { get; set; }
         [Required(ErrorMessage = "Please enter mature size of pet")]
         [Display(Name = "Size at maturity")]
@@ -43,7 +48,9 @@ namespace petrus.ViewModels
         [Required(ErrorMessage = "Please enter adoption fee")]
         public double Fee { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Pet Image")]
         public IFormFile PetImage { get; set; }
+        [Display(Name = "Pet Video")]
         public IFormFile PetVideo { get; set; }
     }
 }
