2ff82a4f2b521499072793a14394fbd990b7bcb2
thatwys <956807158@qq.com>
2021-08-14 23:41:57 +0800

    Models for service amending

---
+++ petrus/Models/ServiceBooking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace petrus.Models
+{
+    public class ServiceBooking
+    {
+        [Key]
+        [Required]
+        [Display(Name = "ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ServiceBookingID { get; set; }
+        public string SpecialRequest { get; set; }
+        [Required]
+        public string RequestedDate { get; set; }
+        [Required]
+        public string PetName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Please enter breed")]
+        public Breed Breed { get; set; }
+        [Required(ErrorMessage = "Please enter gender")]
+        
+        public string UserID { get; set; }
+        public virtual ServiceListing ServiceListing { get; set; }
+
+    }
+   
+    
+    
+}
---
+++ petrus/Models/ServiceListing.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace petrus.Models
+{
+    public class ServiceListing
+    {
+        [Key]
+        [Required]
+        [Display(Name = "ID")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string ServiceListingID { get; set; }
+        [Required]
+        public Species Species { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Price { get; set; }
+        [Required]
+        public string ServiceCategory { get; set; }
+        [Required]
+        public List<DateTime> AvailableDate { get; set; }
+        public string Qualifications { get; set; }
+        public string Video { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public DateTime ListingDate { get; set; }      
+        public string UserID { get; set; } 
+    }
+}
