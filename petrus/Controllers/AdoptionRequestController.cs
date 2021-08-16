c501609469a0213948c24e03fa791b20e3345cfa
Charlotte Wong <charlotte.wonggg@gmail.com>
2021-08-16 09:19:56 +0800

    view adoption request listing
    view adoption request listing

--- petrus/Controllers/AdoptionRequestController.cs
+++ petrus/Controllers/AdoptionRequestController.cs
@@ -1,16 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using petrus.Data;
+using petrus.Models;
+using petrus.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace petrus.Controllers
 {
     public class AdoptionRequestController : Controller
     {
-        public IActionResult Index()
+        private readonly petrusDb dbContext;
+
+        public AdoptionRequestController(petrusDb context)
         {
-            return View();
+            dbContext = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<RequestDetailsViewModel> requestDetailsVM = new List<RequestDetailsViewModel>();
+
+            List<AdoptionRequest> adoptionRequestList = await dbContext.AdoptionRequests.Include(x => x.User).Where(u => u.User.UserID == "1").OrderByDescending(t => t.RequestDate).ToListAsync();
+
+            foreach(AdoptionRequest item in adoptionRequestList)
+            {
+                RequestDetailsViewModel individual = new RequestDetailsViewModel
+                {
+                    AdoptionRequest = item,
+                    AdoptionListing = await dbContext.AdoptionListings.Where(x => x.AdoptionListingID == item.AdoptionListing.AdoptionListingID).ToListAsync()
+                };
+
+                requestDetailsVM.Add(individual);
+
+            }
+
+            return View(requestDetailsVM.OrderByDescending(r => r.AdoptionRequest.RequestDate).ToList());
         }
     }
 }
