a9a8f958ce79ac32e11b20cf332343be96e7247d
ZhengguangHan <hanzh93@gmail.com>
2021-08-17 15:55:02 +0800

    Updated Adoption Request

--- petrus/wwwroot/js/ValidateAdoptionListing.js
+++ petrus/wwwroot/js/ValidateAdoptionListing.js
@@ -5,12 +5,14 @@
 
 
 function ValidateSelected() {
-    var x = document.forms["myForm"]["selected"].value;
-    if (x == "") {
+    var x = document.forms["requestForm"]["requestSelected"].value;
+
+    if (x == null|| x=="") {
         alert("Please select a listing");
         return false;
     }
     else {
+        alert(x);
         return true;
     }
 }
