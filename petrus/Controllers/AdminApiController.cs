3697586a06a43d647b0ea2e328edf9a139955d96
truongkimson <60567409+truongkimson@users.noreply.github.com>
2021-08-23 23:26:28 +0800

    add admin api controller

---
+++ petrus/Controllers/AdminApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace petrus.Controllers
+{
+    [ApiController]
+    [Route("api/admin")]
+    public class AdminApiController : Controller
+    {
+        [HttpGet]
+        [Route("adoption-speed")]
+        public IActionResult AdoptionSpeed()
+        {
+            return Ok("Hello world");
+        }
+
+        [HttpGet]
+        [Route("adoptions-by-species")]
+        public IActionResult AdoptionsBySpecies()
+        {
+            return Ok("Hello world - Speices");
+        }
+
+        [HttpGet]
+        [Route("adoptions-by-month")]
+        public IActionResult AdoptionsByMonth()
+        {
+            return Ok("Hello world - Monthly");
+        }
+    }
+}
