11077ed92150bab3da9f43118fc4808d9d2342fc
ZhengguangHan <hanzh93@gmail.com>
2021-08-28 04:01:09 +0800

    Implemented edit for adoption requests through mobile

---
+++ petrus/BindingModel/AdoptionRequestEditBinding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace petrus.BindingModel
+{
+    public class AdoptionRequestEditBinding
+    {
+        public string userId { get; set; }
+        public string adoptionListingId { get; set; }
+        public string adoptionRequestId { get; set; }
+        public string residenceType { get; set; }
+        public int dogsOwned { get; set; }
+        public string description { get; set; }
+    }
+}
